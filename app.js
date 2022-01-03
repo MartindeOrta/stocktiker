08d84477c9598c73964f54dbabc4bce6df0d847f
MartindeOrta <82926690+MartindeOrta@users.noreply.github.com>
2022-01-03 17:59:24 -0300

    bitcoin a dolar
    listo

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-let ws = new WebSocket('wss://stream.binance.com:9443/ws/etheeur@trade');
-
-
+let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
+let stockPriceElement = document.getElementById("stock-price");
+let lastPrice= null;
 ws.onmessage = (event) => {
-    console.log(event.data);
+
+    let stockObject = JSON.parse(event.data);
+    console.log(stockObject.p);
     
+        let price=parseFloat(stockObject.p).toFixed(2);
+        stockPriceElement.innerText = "Precio Bitcoin a dolar: "+ price;
+    stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : lastPrice < price ? 'green' : 'red';
+lastPrice= price;
+
 }
